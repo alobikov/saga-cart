5ab86ebc5571226ca767a252bf575867ece02417
Aleksej Mint 20 <aleksej.lobikov@gmail.com>
2021-09-07 15:35:06 +0300

    don't fetch item details if item alredy in the global store

--- src/store/sagas/itemDetailsSaga.ts
+++ src/store/sagas/itemDetailsSaga.ts
@@ -1,17 +1,20 @@
-import {put, all, call, takeEvery, fork} from 'redux-saga/effects'
+import {put, all, call, takeEvery, fork, select} from 'redux-saga/effects'
 import * as cart from "../cart"
 import * as items from '../items'
 import {ICart, ICartItem, IItem} from "../../types/types";
+import {RootState} from "../index";
 
 export function* loadItemDetails(item: ICartItem) {
-    const {id} = item
+    const {id } = item
     const response: Response = yield fetch('http://localhost:8081/items/' +id)
     const data: IItem[] = yield response.json()
     yield put(items.addOne(data[0]))
 }
 
 export function* itemDetailsSagaWorker({payload}: { payload: ICart }) {
-    yield all(payload.items.map(item =>
+    const items = yield select((state: RootState)=> state.items)
+    const itemsToLoad = payload.items.filter(item => !(item.id in items))
+    yield all(itemsToLoad.map(item =>
         fork(loadItemDetails, item)
     ))
 }
