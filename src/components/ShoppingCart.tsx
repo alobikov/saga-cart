781e374680f8868e3c0a60734db535905f7cf385
Aleksej Mint 20 <aleksej.lobikov@gmail.com>
2021-09-08 16:49:02 +0300

    add memo selector for subTotal price

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -4,24 +4,43 @@ import {RootState} from '../store'
 import styles from './ShoppingCart.module.css'
 import UserInfo from "./UserInfo";
 import CartItemList from "./CartItemList";
+import {createSelector} from '@reduxjs/toolkit'
+import {IItem, ICartItem, IItemPrice} from "../types/types";
 
 const MyComponent = () => {
-    const user = useSelector((state: RootState) => state.user)
+        const user = useSelector((state: RootState) => state.user)
+        const selectSubTotal = createSelector(
+            [(state: RootState) => state.cart.items,
+                state => state.itemsPrice],
+            (cartItems, itemsPrice) => {
+                if (cartItems && itemsPrice) {
+                    return cartItems.reduce((a, c) => a + itemsPrice[c?.id]?.price * c.quantity, 0)
+                }
+            }
+        )
+        const subTotal = useSelector(selectSubTotal)
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.leftColumn}>
-                <h1>SagaCart</h1>
-                <UserInfo user={user}/>
-                <CartItemList />
-            </div>
-            <div className={styles.rightColumn}>
-                <div className={styles.checkoutContainer}>
-                    <button className={styles.btnCheckout}>Checkout</button>
+        return (
+            <div className={styles.container}>
+                <div className={styles.leftColumn}>
+                    <h1>SagaCart</h1>
+                    <UserInfo user={user}/>
+                    <CartItemList/>
+                </div>
+                <div className={styles.rightColumn}>
+
+                    <div className={styles.checkoutContainer}>
+                        <button className={styles.btnCheckout}>Checkout</button>
+                    </div>
+                    {
+                        subTotal ?
+                            <p>{`Sub Total: ${subTotal?.toFixed(2)}`}</p>
+                            : null
+                    }
                 </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+;
 
 export default MyComponent;
